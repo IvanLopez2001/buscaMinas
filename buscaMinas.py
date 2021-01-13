82c7c3f08df128312375e3c7d4349ba610b9ce4a
pandiii <ivandoblefriki@gmail.com>
2021-01-13 14:02:53 -0300

    printea muchas casillas blancas y una linea roja que proximamente sera un tipo de menu

--- buscaMinas.py
+++ buscaMinas.py
@@ -1,22 +1,33 @@
 import pygame
-screen = pygame.display.set_mode((720, 480))
+screen = pygame.display.set_mode((720, 500))
 clock = pygame.time.Clock()
 FPS = 60  # Frames per second.
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-# RED = (255, 0, 0), GREEN = (0, 255, 0), BLUE = (0, 0, 255).
+RED = (100, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
-rect = pygame.Rect((0, 0), (32, 32))
-image = pygame.Surface((32, 32))
+#rect = pygame.Rect((0, 20), (20, 20))
+image = pygame.Surface((20, 20))
 image .fill(WHITE)  
+menu = pygame.Surface((720, 20))
+menu .fill(RED)
 
 while True:
     clock.tick(FPS)
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
-    #screen.fill(BLACK)
-    #screen.blit(image, rect)
-    #pygame.display.update()  # Or pygame.display.flip()
+    screen.fill(BLACK)
+    for i in range(24):
+        for x in range(36):
+            rect = pygame.Rect((20*x, 20*(i+1)), (20, 20))
+            screen.blit(image, rect)
+    screen.blit(menu, [0,0])
+    pygame.display.update() #or pygame.display.flip()
+
+
+
+
