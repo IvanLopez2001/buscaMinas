5d1bcb4eb4afbb55fd33bfd1aa1804c215709873
pandiii <ivandoblefriki@gmail.com>
2021-01-13 22:05:44 -0300

    Cuando haces click en cada cuadrado distingue si hay una mina o no aunque no hace nada, simplemente no pinta de azul el cuadrado, y cuando no hay nada si lo pinta de azul

--- modelo/celda.py
+++ modelo/celda.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (100, 0, 0)
@@ -8,7 +9,7 @@ screen = pygame.display.set_mode((720, 500))
 class Celda():
 
     def __init__(self):
-            self.contenidos = []
+            self.contenidos = {}
 
     def printearCuadros(self, screen, image):
         for i in range(24):
@@ -16,18 +17,28 @@ class Celda():
                 rect = pygame.Rect((20*x, 20*(i+1)), (19, 19))
                 screen.blit(image, rect)
 
+                if random.randint(0 , 25) == 20:
+                    self.contenidos.setdefault(str([20*x, 20*(i+1)]), 1)
+                elif random.randint(0 , 25) != 20:
+                    self.contenidos.setdefault(str([20*x, 20*(i+1)]), 0)
+
+
+
 
 
     def cambiarColor(self, pos, image):
-        image = pygame.Surface((19, 19))
-        image .fill(BLUE) 
-        #Aux . fill(BLUE)
         x = pos[0]
         y = pos[1]
         auxX = x % 20
         auxY = y % 20
         lugar = [(x-auxX), (y-auxY)]
-        screen.blit(image, lugar)
+        print (lugar)
+        auxiliar =str(lugar)
+        if self.contenidos.get(auxiliar) == 0:
+            image = pygame.Surface((19, 19))
+            image .fill(BLUE) 
+            #Aux . fill(BLUE)
+            screen.blit(image, lugar)
 
 
 if __name__ == "__main__":
